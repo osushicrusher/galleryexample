45bc8bf27ec093b970b25714804965ea9982836f
osushicrusher <osushicrusher@gmail.com>
2020-04-05 22:53:23 +0900

    データベースの設定

--- includes/dbh.php
+++ includes/dbh.php
@@ -1,10 +1,25 @@
 <?php
 
-    $servername = "localhost";
+try {
+    // データソースネーム
+    $dsn = 'mysql:dbname=galleryexample;host=localhost;charset=utf8';
+    // ユーザーネーム
     $username = "root";
+    // パスワード
     $password = "";
-    $dbname = "galleryexample";
 
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // データベースハンドラ
+    $dbh = new PDO(
+        $dsn,
+        $username, 
+        $password, 
+        array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // エラー発生時に例外を投げるようにする
+            PDO::ATTR_EMULATE_PREPARES => false // 静的プレースホルダを用いる
+        )
+    );
+} catch (PDOException $e) {
+    $error = $e->getMessage();
+}
 
 ?>
